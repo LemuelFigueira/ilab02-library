b9c6e66aaa81c8186d95ba59c26362d7c5e26f4b
Lemuel Figueira <lemuelfigueira@unigranrio.br>
2022-10-14 21:39:43 -0300

    ajustes tailwind config
    adicionando paleta de cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
       './stories/**/*'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: {
+          500: '#81D8F7',
+          300: '#A1E1F9'
+        },
+        gray: {
+          900: '#121214',
+          800: '#202024',
+          400: '#7C7CBA',
+          200: '#C4C4CC',
+          100: '#E1E1E6'
+        }
+      }
+    },
   },
   plugins: [],
 }
