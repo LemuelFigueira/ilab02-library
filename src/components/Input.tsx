ffc0cad7913242c6628b133f3b67ce2c16d3cd38
Lemuel Figueira <pereiraleleo87@gmail.com>
2022-10-16 03:10:31 -0300

    ajustes input
    +password

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -25,6 +25,35 @@ const emailSvg: React.SVGProps<SVGSVGElement> = (
   </svg>
 );
 
+const lockSvg: React.SVGProps<SVGSVGElement> = (
+  <svg
+    className="fill-gray-400 stroke-gray-400 w-6"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 256 256"
+  >
+    <rect width="256" height="256" stroke="#00000000" fill="none"></rect>
+    <rect
+      x="40"
+      y="88"
+      width="176"
+      height="128"
+      rx="8"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="16"
+    ></rect>
+    <path
+      d="M92,88V52a36,36,0,0,1,72,0V88"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="16"
+    ></path>
+    <circle cx="128" cy="152" r="12"></circle>
+  </svg>
+);
+
 export interface InputProps
   extends Omit<
     React.InputHTMLAttributes<HTMLInputElement>,
@@ -32,7 +61,7 @@ export interface InputProps
   > {
   label: string;
   size?: 'sm' | 'md' | 'lg' | 'full';
-  type: 'email' | 'text';
+  type?: 'email' | 'text' | 'password';
   onClick?: () => void;
 }
 
@@ -46,6 +75,7 @@ const sizes = {
 const icons = {
   email: emailSvg,
   text: null,
+  password: lockSvg,
 };
 
 export const Input = ({
@@ -66,6 +96,7 @@ export const Input = ({
             id="input"
             className={`select-none focus:outline-none w-full text-white placeholder:text-gray-400 font-normal text-sm rounded bg-transparent`}
             placeholder={placeholder}
+            type={type}
             {...props}
           />
         </div>
