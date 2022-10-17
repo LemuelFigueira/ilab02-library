9749b2d1ca170bd9b2cc9400694f17d1a6b10fff
Lemuel Figueira <pereiraleleo87@gmail.com>
2022-10-17 04:27:11 -0300

    ajuste checkbox
    unique id

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -19,13 +19,13 @@ export const Checkbox = ({
 
   return (
     <div className="flex items-center mb-4">
-      <CheckboxPrimitive.Root checked={checked} onCheckedChange={onCheckedChange} id="c1" className="w-6 h-6 bg-zinc-900 flex items-center justify-center rounded-lg">
+      <CheckboxPrimitive.Root checked={checked} onCheckedChange={onCheckedChange} id={id} className="w-6 h-6 bg-zinc-900 flex items-center justify-center rounded-lg">
         <CheckboxPrimitive.Indicator className="text-cyan-500">
           <Check size={18}/>
         </CheckboxPrimitive.Indicator>
       </CheckboxPrimitive.Root>
       <label
-        htmlFor="c1"
+        htmlFor={id}
         className="select-none cursor-pointer ml-2 font-normal text-gray-200 dark:text-gray-300 text-sm"
       >
         {children}
