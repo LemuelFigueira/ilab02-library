27780e0b6de45dd9c3a80b882a86ba57e1c1db0c
Lemuel Figueira <pereiraleleo87@gmail.com>
2022-10-17 04:27:45 -0300

    incrementando versão
    1.2.0

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { Button } from './components/Button';
 export { Label } from './components/Label';
 export { Heading } from './components/Heading';
 export { Input } from './components/Input';
+export { Checkbox } from './components/Checkbox';
